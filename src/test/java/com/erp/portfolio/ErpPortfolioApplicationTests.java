cea4dcb64e294130088116411ca6624779b0eb34
Carlos Mamani <carlosfernando21334@gmail.com>
2024-02-14 11:39:51 -0300

    Creacion de proyecto

---
+++ src/test/java/com/erp/portfolio/ErpPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
