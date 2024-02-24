1c651998c10b55b3091002b769c7316cf24e17a0
Carlos Mamani <carlosfernando21334@gmail.com>
2024-02-23 22:25:07 -0300

    DataSource configurado

---
+++ src/main/java/com/erp/portfolio/configuration/DataSourceConfiguration.java
@@ -0,0 +1,35 @@
+package com.erp.portfolio.configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    private final String classNameDriver;
+    private final String password;
+    private final String username;
+    private final String url;
+
+    public DataSourceConfiguration(@Value("${datasource.classnamedriver}") String classNameDriver,@Value("${datasource.password}") String password,
+                                   @Value("${datasource.username}")String username, @Value("${datasource.url}")String url) {
+        this.classNameDriver = classNameDriver;
+        this.password = password;
+        this.username = username;
+        this.url = url;
+    }
+
+    @Bean("mysqlProjectsDataSource")
+    public DataSource getDataSource()
+    {
+        return DataSourceBuilder.create().
+                driverClassName(classNameDriver)
+                .password(password)
+                .url(url)
+                .username(username)
+                .build();
+    }
+
+}
